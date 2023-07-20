252b072ac3480137cb53a14ec3ec22afb877a019
khushalGrover <khushal15grover@gmail.com>
2023-07-20 23:06:57 +0530

    everything workin fine

--- Spectral_Shadows_Project/Assets/Spectral_Shadows/Script/dayNightManager.cs
+++ Spectral_Shadows_Project/Assets/Spectral_Shadows/Script/dayNightManager.cs
@@ -4,41 +4,79 @@ using UnityEngine;
 
 public class dayNightManager : MonoBehaviour
 {
+    // Reference
+    [SerializeField] private GameObject[] _pointLights;
+    [SerializeField] private GameObject[] _spotLight;
 
-    public GameObject[] lights;
+    // variables
+    /* public Material material;
+     public Color _emissionColorValue;
+     public float desiredValue;
+     public float _intensity;
+     public float decrease; */
+
+    public Color _darkFogColor;
+    public Color _lightFogColor;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         
+        // NOT WROKING PROPERY
+        //material.SetVector("_EmissionColor", _emissionColorValue * _intensity);
+        //_intensity = _intensity - decrease;
+       //  material.SetColor("_EmissionColor", _emissionColorValue * Mathf.Pow(2, _intensity));
+
+        // https://forum.unity.com/threads/change-a-materials-emission-color-intensity-property.611206/
+
     }
+
 
     public void lightsOn()
     {
-        foreach (GameObject light in lights)
+        RenderSettings.ambientIntensity = 0.2f;
+        RenderSettings.fogColor = _darkFogColor;
+        RenderSettings.fogDensity = 0.03f;
+
+        foreach ( GameObject spotLight in _spotLight )
         {
-            if (light != null)
+            if ( spotLight != null )
             {
-                light.gameObject.SetActive(false);
+                spotLight.SetActive( false );
+            }
+        }
+
+        foreach (GameObject pointLight in _pointLights)
+        {
+            if (pointLight != null)
+            {
+                pointLight.gameObject.SetActive(false);
+                
             }
         }
     }
 
     public void lightsOff() 
     {
-        foreach(GameObject light in lights)
+        RenderSettings.ambientIntensity = 0.5f;
+        RenderSettings.fogColor = _lightFogColor;
+        RenderSettings.fogDensity = 0.01f;
+
+        foreach (GameObject pointLight in _pointLights)
         { 
-            if (light != null)
+            if (pointLight != null)
             {
-                light.gameObject.SetActive(true);
+                pointLight.gameObject.SetActive(true);
             }    
+        }
+
+        foreach (GameObject spotLight in _spotLight)
+        {
+            if (spotLight != null)
+            {
+                spotLight.SetActive(true);
+            }
         }
     }
 
