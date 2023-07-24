8f7e88a03ccd782cc475564831fdf060def59003
khushalGrover <khushal15Grover@gmail.com>
2023-07-24 17:49:57 +0530

    prototypr READY

--- Spectral_Shadows_Project/Assets/Spectral_Shadows/Script/dayNightManager.cs
+++ Spectral_Shadows_Project/Assets/Spectral_Shadows/Script/dayNightManager.cs
@@ -7,6 +7,8 @@ public class dayNightManager : MonoBehaviour
     // Reference
     [SerializeField] private GameObject[] _pointLights;
     [SerializeField] private GameObject[] _spotLight;
+    [SerializeField] private GameObject ghost;
+
 
     // variables
     /* public Material material;
@@ -14,10 +16,15 @@ public class dayNightManager : MonoBehaviour
      public float desiredValue;
      public float _intensity;
      public float decrease; */
-
     public Color _darkFogColor;
     public Color _lightFogColor;
 
+
+    void Start()
+    {
+        lightsOff();
+        Debug.Log("Light on default");
+    }
 
 
     void Update()
@@ -35,10 +42,17 @@ public class dayNightManager : MonoBehaviour
 
     public void lightsOn()
     {
+        // environment lights setting
         RenderSettings.ambientIntensity = 0.2f;
         RenderSettings.fogColor = _darkFogColor;
         RenderSettings.fogDensity = 0.03f;
 
+        // to do add fade in effect or dissolve effect
+        ghost.SetActive(true);
+
+        // sounds and vfx
+
+        // celling spot light
         foreach ( GameObject spotLight in _spotLight )
         {
             if ( spotLight != null )
@@ -46,7 +60,7 @@ public class dayNightManager : MonoBehaviour
                 spotLight.SetActive( false );
             }
         }
-
+        // lamp lights
         foreach (GameObject pointLight in _pointLights)
         {
             if (pointLight != null)
@@ -59,10 +73,17 @@ public class dayNightManager : MonoBehaviour
 
     public void lightsOff() 
     {
+        // environment lights setting
         RenderSettings.ambientIntensity = 0.5f;
         RenderSettings.fogColor = _lightFogColor;
         RenderSettings.fogDensity = 0.01f;
 
+        // to do add fade in effect or dissolve effect and give random rotation to spot light
+        ghost.SetActive(false);
+
+        // sounds and vfx
+
+        // celling spot light
         foreach (GameObject pointLight in _pointLights)
         { 
             if (pointLight != null)
@@ -70,7 +91,7 @@ public class dayNightManager : MonoBehaviour
                 pointLight.gameObject.SetActive(true);
             }    
         }
-
+        // lamp lights
         foreach (GameObject spotLight in _spotLight)
         {
             if (spotLight != null)
